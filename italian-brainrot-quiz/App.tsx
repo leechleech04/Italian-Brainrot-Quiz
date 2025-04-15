5d449ffdcb4c90de6ff93665d588e8621e2aa3a6
Lee Chanhyeong <lch2023@naver.com>
2025-04-15 09:14:36 +0000

    Init create-expo-app

---
+++ italian-brainrot-quiz/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import { Text } from 'react-native';
+
+const Container = styled.View``;
+
+const App = () => {
+  return (
+    <Container>
+      <Text>Italian Brainrot Quiz</Text>
+    </Container>
+  );
+};
+
+export default App;

28874ee240ed6993ad8f24d249df6867fa5549ca
Lee Chanhyeong <lch2023@naver.com>
2025-04-16 10:36:37 +0000

    Feat Title Animated

--- italian-brainrot-quiz/screens/Home.tsx
+++ italian-brainrot-quiz/screens/Home.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import colors from '../colors';
+import { Animated, TouchableWithoutFeedback } from 'react-native';
+import { useEffect, useRef } from 'react';
 
 const Container = styled.View`
   flex: 1;
@@ -13,14 +15,49 @@ const Title = styled.Text`
   color: ${colors.beige};
   text-align: center;
   font-family: 'PoetsenOne-Regular';
+  line-height: 60px;
 `;
 
 const Home = () => {
+  const titleScale = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.spring(titleScale, {
+      toValue: 1,
+      bounciness: 15,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
     <Container>
-      <Title>
-        Italian{'\n'}Brainrot{'\n'}Quiz
-      </Title>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Animated.sequence([
+            Animated.spring(titleScale, {
+              toValue: 0.8,
+              bounciness: 0,
+              speed: 50,
+              useNativeDriver: true,
+            }),
+            Animated.spring(titleScale, {
+              toValue: 1,
+              bounciness: 15,
+              useNativeDriver: true,
+            }),
+          ]).start();
+        }}
+      >
+        <Animated.Text
+          style={{
+            transform: [{ scale: titleScale }],
+          }}
+        >
+          <Title>
+            Italian{'\n'}Brainrot{'\n'}Quiz
+          </Title>
+        </Animated.Text>
+      </TouchableWithoutFeedback>
     </Container>
   );
 };
